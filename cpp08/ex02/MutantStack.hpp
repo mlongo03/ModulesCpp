60a3be505963e5fcad5b41e3e493e4c870f99295
Manuele Longo <manuele.lng03@gmail.com>
2024-02-23 18:32:41 +0100

    cpp08 ex02

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -11,14 +11,42 @@ class MutantStack : public std::stack<T, container_type>
 {
 	public:
 		typedef typename container_type::iterator	iterator;
-		iterator	begin()
-		{
-			return (this->c.begin());
-		};
-		iterator	end()
-		{
-			return (this->c.end());
-		};
+		MutantStack();
+		MutantStack(const MutantStack& toCopy);
+		~MutantStack();
+		MutantStack&	operator=(const MutantStack& toCopy);
+		iterator	begin();
+		iterator	end();
+};
+
+template<typename T, typename container_type>
+MutantStack<T, container_type>::MutantStack() : std::stack<T, container_type>() {}
+
+template<typename T, typename container_type>
+MutantStack<T, container_type>::MutantStack(const MutantStack& toCopy) : std::stack<T, container_type>(toCopy) {}
+
+template<typename T, typename container_type>
+MutantStack<T, container_type>::~MutantStack() {}
+
+template<typename T, typename container_type>
+MutantStack<T, container_type>&	MutantStack<T, container_type>::operator=(const MutantStack<T, container_type>& toCopy)
+{
+	if (this == &toCopy)
+		return *this;
+	this->c = toCopy.c;
+	return *this;
+}
+
+template<typename T, typename container_type>
+typename MutantStack<T, container_type>::iterator	MutantStack<T, container_type>::begin()
+{
+	return (this->c.begin());
+};
+
+template<typename T, typename container_type>
+typename MutantStack<T, container_type>::iterator	MutantStack<T, container_type>::end()
+{
+	return (this->c.end());
 };
 
 #endif
