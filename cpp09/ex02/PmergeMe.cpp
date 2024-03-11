aca93a5d154e97b8b972a09394ea68a438bd04c4
mlongo03 <manuele.lng03@gmail.com>
2024-03-11 18:28:10 +0100

    cpp09 improve insert

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -60,50 +60,119 @@ void	PmergeMe::load_numbers(char **argv)
 
 void	PmergeMe::sort_list()
 {
-	std::list<int> main_chain;
-	std::list<int> second_chain;
+}
+
+std::vector<int> create_pend(std::vector<std::vector<int> > chain, int OddNumber, int recursion_depth)
+{
+	std::vector<int> pend;
+
+	// std::cout << "\nrecursion depth = " << recursion_depth << std::endl;
+	for (size_t i = 0; i < chain.size(); i++)
+		pend.push_back(chain[i][recursion_depth + 1]);
+	if (OddNumber != -1)
+		pend.push_back(OddNumber);
+
+	return pend;
+}
+
+std::vector<std::vector<int> > merge_insertion_vector(std::vector<std::vector<int> > input_chain, int recursion_depth)
+{
+	std::vector<std::vector<int> > main_chain;
+	std::vector<std::vector<int> > result;
+	std::vector<int> pend;
+
 	int OddNumber = -1;
 
-	if (numbers.size() % 2 != 0)
+	if (input_chain.size() % 2 != 0)
 	{
-		OddNumber = numbers.back();
-		numbers.pop_back();
+		OddNumber = input_chain.back()[0];
+		input_chain.pop_back();
 	}
 
-	for (size_t i = 0; i < numbers.size(); i += 2)
+	(void)OddNumber;
+
+	for (size_t i = 0; i < input_chain.size(); i += 2)
 	{
-		if (numbers[i] > numbers[i + 1])
+		// std::cout << input_chain[i][0] << " > " << input_chain[i + 1][0] << std::endl;
+		if (input_chain[i][0] > input_chain[i + 1][0])
 		{
-			main_chain.push_back(numbers[i]);
-			second_chain.push_back(numbers[i + 1]);
+			main_chain.push_back(input_chain[i]);
+			main_chain.back().push_back(input_chain[i + 1][0]);
+			main_chain.back().push_back(i + 1);
+			// input_chain[i].push_back(input_chain[i + 1][0]);
 		}
 		else
 		{
-			main_chain.push_back(numbers[i + 1]);
-			second_chain.push_back(numbers[i]);
+			main_chain.push_back(input_chain[i + 1]);
+			main_chain.back().push_back(input_chain[i][0]);
+			main_chain.back().push_back(i);
+			// input_chain[i + 1].push_back(input_chain[i][0]);
+			// main_chain.push_back(input_chain[i + 1]);
 		}
 	}
 
-	if (OddNumber >= 0)
-		second_chain.push_back(OddNumber);
+	std::cout << "all" << std::endl;
+	for (size_t i = 0; i < main_chain.size(); i++)
+	{
+		for (size_t j = 0; j < main_chain[i].size(); j++)
+			std::cout << main_chain[i][j] << " ";
+		std::cout << std::endl;
+	}
 
-	std::cout << "original list" << std::endl;
-	for (size_t i = 0; i < numbers.size(); i++)
-		std::cout << numbers[i] << " ";
-	std::cout << std::endl;
-	std::cout << "main chain" << std::endl;
-	for (std::list<int>::iterator it = main_chain.begin(); it != main_chain.end(); it++)
-		std::cout << *it << " ";
-	std::cout << std::endl;
-	std::cout << "second chain" << std::endl;
-	for (std::list<int>::iterator it = second_chain.begin(); it != second_chain.end(); it++)
-		std::cout << *it << " ";
-	std::cout << std::endl;
+	// std::cout << "main chain" << std::endl;
+	// for (size_t i = 0; i < main_chain.size(); i++)
+	// 	std::cout << main_chain[i][0] << " ";
+	// std::cout << std::endl;
+
+	if (input_chain.size() != 2)
+		result = merge_insertion_vector(main_chain, recursion_depth + 1);
+	else
+	{
+		std::vector<std::vector<int> >::iterator it;
+		it = main_chain.begin();
+		std::vector<int>	tmp;
+		tmp.push_back(main_chain[0][main_chain[0].size() - 2]);
+		tmp.push_back(main_chain[0][main_chain[0].size() - 1]);
+		it = main_chain.insert (it , input_chain[1]);
+		return main_chain;
+	}
+
+	// pend = create_pend(result, OddNumber, recursion_depth);
+	// std::cout << "result -> ";
+	// for (size_t i = 0; i < result.size(); i++)
+	// 	std::cout << result[i][0] << " ";
+	// std::cout << std::endl;
+	std::cout << "result" << std::endl;
+	for (size_t i = 0; i < result.size(); i++)
+	{
+		for (size_t j = 0; j < result[i].size(); j++)
+			std::cout << result[i][j] << " ";
+		std::cout << std::endl;
+	}
+
+	// for (size_t i = 0; i < pend.size(); i++)
+	// 	std::cout << pend[i] << " ";
+	// std::cout << std::endl;
+
+	return main_chain;
 }
 
 void	PmergeMe::sort_vector()
 {
+	std::vector<std::vector<int> >	first_chain;
+	std::vector<int>				tmp;
 
+	for (size_t i = 0; i < numbers.size(); i++)
+	{
+		tmp.push_back(numbers[i]);
+		first_chain.push_back(tmp);
+		tmp.clear();
+	}
+	std::cout << "first chain" << std::endl;
+	for (size_t i = 0; i < first_chain.size(); i++)
+		std::cout << first_chain[i][0] << " ";
+	std::cout << std::endl;
+	merge_insertion_vector(first_chain, 0);
 }
 
 const char* PmergeMe::NotAnInteger::what() const throw()
